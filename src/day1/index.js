5fb20f47eb27ca3dead43f2b692f4d531ed7bb7b
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2020-12-02 16:20:22 +0000

    refactor: day 1

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,87 +1,44 @@
 const { toSortedNumberArray } = require('../lib/input');
 
-/**
- * --- Day 1: Report Repair ---
- * After saving Christmas five years in a row, you've decided to take a vacation
- * at a nice resort on a tropical island. Surely, Christmas will go on without
- * you.
- *
- * The tropical island has its own currency and is entirely cash-only. The gold
- * coins used there have a little picture of a starfish; the locals just call
- * them stars. None of the currency exchanges seem to have heard of them, but
- * somehow, you'll need to find fifty of these coins by the time you arrive so
- * you can pay the deposit on your room.
- *
- * To save your vacation, you need to get all fifty stars by December 25th.
- *
- * Collect stars by solving puzzles. Two puzzles will be made available on each
- * day in the Advent calendar; the second puzzle is unlocked when you complete
- * the first. Each puzzle grants one star. Good luck!
- *
- * Before you leave, the Elves in accounting just need you to fix your expense
- * report (your puzzle input); apparently, something isn't quite adding up.
- *
- * Specifically, they need you to find the two entries that sum to 2020 and then
- * multiply those two numbers together.
- *
- * For example, suppose your expense report contained the following:
- *
- *   1721
- *   979
- *   366
- *   299
- *   675
- *   1456
- *
- * In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying
- * them together produces 1721 * 299 = 514579, so the correct answer is 514579.
- *
- * Of course, your expense report is much larger. Find the two entries that sum
- * to 2020; what do you get if you multiply them together?
- */
-const part1 = (input) => {
-  const entries = toSortedNumberArray(input);
-  const entrySet = new Set(entries);
+// https://en.wikipedia.org/wiki/3SUM
+const twoSum = (sum, numbers) => {
+  const set = new Set(numbers);
 
-  for (let i = 0; i < entries.length; i++) {
-    const entry = 2020 - entries[i];
+  for (const first of numbers) {
+    const second = sum - first;
 
-    if (entrySet.has(entry)) {
-      return entries[i] * entry;
+    if (set.has(second)) {
+      return first * second;
     }
   }
 
   return null;
 };
 
-/**
- * --- Part Two ---
- * The Elves in accounting are thankful for your help; one of them even offers
- * you a starfish coin they had left over from a past vacation. They offer you a
- * second one if you can find three numbers in your expense report that meet the
- * same criteria.
- *
- * Using the above example again, the three entries that sum to 2020 are 979,
- * 366, and 675. Multiplying them together produces the answer, 241861950.
- *
- * In your expense report, what is the product of the three entries that sum to
- * 2020?
- */
-const part2 = (input) => {
-  const entries = toSortedNumberArray(input);
-  const entrySet = new Set(entries);
+const threeSum = (sum, numbers) => {
+  const set = new Set(numbers);
 
-  for (let i = 0; i < entries.length; i++) {
-    for (let j = 0; j < entries.length; j++) {
-      const entry = 2020 - entries[i] - entries[j];
+  for (const first of numbers) {
+    for (const second of numbers) {
+      const third = sum - first - second;
 
-      if (entrySet.has(entry)) {
-        return entries[i] * entries[j] * entry;
+      if (set.has(third)) {
+        return first * second * third;
       }
     }
   }
 
   return null;
+};
+
+// Puzzle answer: 73371
+const part1 = (input) => {
+  return twoSum(2020, toSortedNumberArray(input));
+};
+
+// Puzzle answer: 27642310
+const part2 = (input) => {
+  return threeSum(2020, toSortedNumberArray(input));
 };
 
 module.exports = { part1, part2 };
