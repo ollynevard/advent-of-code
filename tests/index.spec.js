ed2252947c13e77bbf1d68ccbafe32b798a03581
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2020-12-04 15:25:07 +0000

    feat: add aoc cli

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -5,13 +5,22 @@ const fs = require('fs');
 const path = require('path');
 const { assert } = require('chai');
 
+const { YEAR, DAY, PART } = process.env;
 const puzzlePath = path.join(__dirname, '../src/puzzles');
 
 fs.readdirSync(puzzlePath).forEach((year) => {
+  if (YEAR !== 'undefined' && YEAR !== year) {
+    return;
+  }
+
   const yearPath = path.join(puzzlePath, year);
 
   describe(year, () => {
     fs.readdirSync(yearPath).forEach((day) => {
+      if (DAY !== 'undefined' && DAY !== day) {
+        return;
+      }
+
       const dayPath = path.join(yearPath, day);
       const testPath = path.join(dayPath, 'test');
       const solutions = require(dayPath);
@@ -19,6 +28,10 @@ fs.readdirSync(puzzlePath).forEach((year) => {
 
       describe(`Day ${day}`, () => {
         allTests.forEach((tests, part) => {
+          if (PART !== 'undefined' && PART !== String(part + 1)) {
+            return;
+          }
+
           describe(`Part ${part + 1}`, () => {
             tests.forEach((test, index) => {
               it(`Test ${index + 1}: Expected ${test.expected}`, () => {
