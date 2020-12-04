0d2acb6e4f0262c87bfab81e73f17daf7ce6056f
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2020-12-04 12:11:10 +0000

    feat: add day 4 solution

---
+++ src/puzzles/2020/04/index.js
@@ -0,0 +1,66 @@
+const { load } = require('../../../lib/input');
+
+const getPassportFromInput = (input) => input.split('\n\n');
+
+const getPassportFields = (passport) =>
+  Object.fromEntries(
+    passport.match(/([^\s]+):([^\s]+)/g).map((field) => field.split(':'))
+  );
+
+const hasValidFields = (fields) =>
+  ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'].reduce(
+    (isValid, field) => isValid && field in fields,
+    true
+  );
+
+const isValidDate = (date, from, to) => date >= from && date <= to;
+
+const isValidHeight = (height) => {
+  const [, value, unit] = height.match(/^(\d+)(cm|in)$/) || [];
+
+  if (unit === 'cm') {
+    return value >= 150 && value <= 193;
+  }
+
+  return value >= 59 && value <= 76;
+};
+
+const isValidHairColor = (color) => color.match(/^#[0-9a-f]{6}$/) !== null;
+
+const isValidEyeColor = (color) =>
+  color.match(/^(amb|blu|brn|gry|grn|hzl|oth)$/) !== null;
+
+const isValidPassportId = (id) => id.match(/^\d{9}$/) !== null;
+
+// Puzzle answer: 250
+const part1 = (input) => {
+  return getPassportFromInput(input).reduce(
+    (validPassports, passport) =>
+      validPassports + hasValidFields(getPassportFields(passport)),
+    0
+  );
+};
+
+// Puzzle answer: 158
+const part2 = (input) => {
+  return getPassportFromInput(input).reduce((validPassports, passport) => {
+    const fields = getPassportFields(passport);
+
+    return (
+      validPassports +
+      (hasValidFields(fields) &&
+        isValidDate(fields.byr, 1920, 2002) &&
+        isValidDate(fields.iyr, 2010, 2020) &&
+        isValidDate(fields.eyr, 2020, 2030) &&
+        isValidHeight(fields.hgt) &&
+        isValidHairColor(fields.hcl) &&
+        isValidEyeColor(fields.ecl) &&
+        isValidPassportId(fields.pid))
+    );
+  }, 0);
+};
+
+module.exports = { part1, part2 };
+
+console.log(part1(load(__dirname + '/input.txt')));
+console.log(part2(load(__dirname + '/input.txt')));
