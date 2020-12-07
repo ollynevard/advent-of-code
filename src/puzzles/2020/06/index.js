11a886913f88d7416f5d2a911879fca7658f87c5
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2020-12-07 21:25:48 +0000

    refactor: day 6

--- src/puzzles/2020/06/index.js
+++ src/puzzles/2020/06/index.js
@@ -1,29 +1,32 @@
-// Puzzle answer: 6437
-const part1 = (input) => {
-  return input
-    .split('\n\n')
-    .map((group) => group.match(/(\w)/g))
-    .reduce((count, answers) => count + new Set(answers).size, 0);
+const getSummary = (answers) =>
+  answers.reduce((map, item) => {
+    [...item].forEach((question) =>
+      map.set(question, map.get(question) + 1 || 1)
+    );
+    return map;
+  }, new Map());
+
+const getGroups = (input) => {
+  return input.split('\n\n').map((group) => {
+    const answers = group.split('\n');
+    return { answers, summary: getSummary(answers) };
+  });
 };
+
+// Puzzle answer: 6437
+const part1 = (input) =>
+  getGroups(input).reduce((count, group) => count + group.summary.size, 0);
 
 // Puzzle answer: 3229
 const part2 = (input) => {
-  return input.split('\n\n').reduce((count, group) => {
-    const people = group.split('\n');
-    const answers = people.reduce((map, person) => {
-      person.split('').forEach((letter) => {
-        const letterCount = map.has(letter) ? map.get(letter) + 1 : 1;
-        map.set(letter, letterCount);
-      });
-      return map;
-    }, new Map());
-
-    const distinctAnswers = [...answers].filter(
-      (answer) => answer[1] >= people.length
-    );
-
-    return count + distinctAnswers.length;
-  }, 0);
+  return getGroups(input).reduce(
+    (count, group) =>
+      count +
+      [...group.summary].filter(
+        (answerCount) => answerCount[1] >= group.answers.length
+      ).length,
+    0
+  );
 };
 
 module.exports = { part1, part2 };
